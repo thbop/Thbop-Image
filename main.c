558bc98a4d2dcfcf9e806472eb036bf0e6a0a2d9
Thbop <75861321+thbop@users.noreply.github.com>
2024-03-12 21:56:06 -0400

    some structures

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Color {
+    unsigned char r, g, b;
+} typedef Color;
+
+void printColor( Color color ) {
+    printf( "{ %x, %x, %x }\n", color.r, color.g, color.b );
+}
+
+struct Image {
+    int width, height;
+    Color* colors;
+    unsigned char* pixels; // Points to the bytes representing color pointers; len = width * height
+} typedef Image;
+
+
+
+
+
+
 int main() {
-    printf("Hello World!\n");
+    Color a = { 255, 56, 0 };
+    printColor(a);
+    
     return 0;
 }
